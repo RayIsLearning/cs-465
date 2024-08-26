058b0060de53bcffbb5718413c1c6d15b3773346
RayIsLearning <64932993+RayIsLearning@users.noreply.github.com>
2024-08-25 22:45:15 -0400

    Add files via upload

---
+++ src/app/data/trips.ts
@@ -0,0 +1,32 @@
+export const trips = [
+    {
+        "name" : "Gale Reef",
+        "image" : "reef1.jpg",
+        "code" : "GALR210214",
+        "length" : "4 nights / 5 days",
+        "start" : "2021-02-14T08:00:00Z",
+        "resort" : "Emerald Bay, 3 stars",
+        "perPerson" : "799.00",
+        "description" : "Gale Reef Sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollis vel vestibulum orci gravida. Vestibulum sit amet porttitor odio. Nulla facilisi. Fusce at pretium felis."
+    },
+    {
+        "name" : "Dawsons Reef",
+        "image" : "reef2.jpg",
+        "code" : "DAWR210315",
+        "length" : "4 nights / 5 days",
+        "start" : "2021-03-15T08:00:00Z",
+        "resort" : "Blue Lagoon, 4 stars",
+        "perPerson" : "1199.00",
+        "description" : "Dawsons Reef Integer magna leo, posuere et dignissim vitae, porttitor at odio. Pellentesque a metus nec magna placerat volutpat. Nunc nisi mi, elementum sit amet aliquet quis, tristique quis nisl. Curabitur odio lacus, blandit ut hendrerit"
+    },
+    {
+        "name" : "Claires Reef",
+        "image" : "reef3.jpg",
+        "code" : "CLAR210621",
+        "length" : "4 nights / 5 days",
+        "start" : "2021-06-21T08:00:00Z",
+        "resort" : "Coral Sands, 5 stars",
+        "perPerson" : "1999.00",
+        "description" : "Claires Reef Donec sed felis risus. Nulla facilisi. Donec a orci tellus, et auctor odio. Fusce ac orci nibh, quis semper arcu. Cras orci neque, euismod et accumsan ac, sagittis molestie lorem. Proin odio sapien, elementum at tempor non."
+    }
+];
---
+++ src/app/services/trip-data.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Observable} from 'rxjs';
+import { Trip } from '../models/trip';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TripDataService {
+
+  constructor() { }
+}
